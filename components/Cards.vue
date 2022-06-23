4248627f444b01cf82e5a9b20b5808f628684712
Danil <shoxieshox88@gmail.com>
2022-06-23 17:58:44 +0300

    wip

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,15 +1,26 @@
 <template>
   <v-container class="d-flex justify-space-around flex-wrap">
-    <v-card
-      class="my-3 mx-1 column"
-      v-for="(game, idx) in games"
-      :key="game.id"
-    >
-      <v-img height="250" :src="game.background_image" />
-      <v-card-title> {{ game.name}} </v-card-title>
-      <v-card-subtitle> {{ game.tags[idx].name}} </v-card-subtitle>
-      <v-divider />
-      <v-card-text> Text </v-card-text>
+    <v-card class="my-3 mx-1 column d-flex flex-column justify-space-evenly pb-4" v-for="game in games" :key="game.id">
+      <v-img width="300" :src="game.background_image" />
+      <v-card-title> {{ game.name }} </v-card-title>
+      <v-chip-group column class="ml-3">
+        <v-chip v-for="tag in game.tags.slice(0, 4)" :key="tag.id">
+          {{ tag.name }}
+        </v-chip>
+      </v-chip-group>
+      <div class="d-flex">
+        <v-rating
+          class="ml-2"
+          background-color="grey lighten-2"
+          color="warning"
+          half-increments
+          readonly
+          length="5"
+          size="20"
+          :value="game.rating"
+        />
+        <span class="ml-5 my-auto"> {{ game.rating }} </span>
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -20,6 +31,9 @@ import { mapState } from "vuex";
 
 export default Vue.extend({
   name: "Cards",
+  data() {
+    return {};
+  },
   computed: {
     ...mapState({ games: "data" }),
   },
@@ -27,5 +41,7 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.column { width: 25% }
+.column {
+  width: 25%;
+}
 </style>
